9542cdd9b7d0561a4cceae9204f51f159ff44809
Saurabh Pujari <saurabhpuj99@gmail.com>
2020-10-02 16:05:04 +0530

    update done with gitmeup

---
+++ bin/get.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node 
+
+const axios = require('axios');
+ 
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
